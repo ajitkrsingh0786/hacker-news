e31daf3cba06945f8d5d7914e3164d1ac4b2ba50
Ajit Kumar Singh <ajit.singh@mountblue.tech>
2020-12-30 07:09:59 +0530

    Home page

--- src/main/java/com/example/hackernews/controller/PostController.java
+++ src/main/java/com/example/hackernews/controller/PostController.java
@@ -5,12 +5,9 @@ import com.example.hackernews.services.PostService;
 import com.example.hackernews.services.PostServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class PostController implements PostServiceInterface {
 
     PostService postService;
@@ -19,6 +16,14 @@ public class PostController implements PostServiceInterface {
     public void setPostService(PostService postService) {
         this.postService = postService;
     }
+
+    @RequestMapping("/")
+     public String showHomePage(){
+        return "html/index";
+    }
+
+
+
 
     @PostMapping("submitPost")
     public String submitPost(@ModelAttribute Post post,
